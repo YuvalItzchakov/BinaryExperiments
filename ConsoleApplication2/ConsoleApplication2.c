7f85708725bbd559c90367d4ba1429652e770ad6
Yuval Itzchakov <yuval.itzchakov@clicktale.com>
2016-10-15 16:18:51 +0300

    Socket can now read values

--- ConsoleApplication2/ConsoleApplication2.c
+++ ConsoleApplication2/ConsoleApplication2.c
@@ -21,7 +21,10 @@ int main()
 	WSAStartup(MAKEWORD(2, 0), &wsaData);
 
 	dest.sin_family = AF_INET;
+	dest.sin_port = htons(80);
+	dest.sin_addr.s_addr = INADDR_ANY;
 	thisSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
 	int returnStatus;
 	if (thisSocket < 0)
 	{
@@ -33,7 +36,42 @@ int main()
 		returnStatus = 1;
 	}
 
+	if (bind(thisSocket, (struct sockaddr *)&dest, sizeof(dest)) < 0) close_socket_with_error_num(thisSocket, "\nFailed to bind socket at port: %d\n", ntohs((u_short)dest.sin_port));
+
+	printf("Listening on %u..", ntohs((u_short)dest.sin_port));
+	if (listen(thisSocket, 1) < 0) close_socket_with_error(thisSocket, "\nFailed to listen to socket", "");
+
+	struct sockaddr_in clientAddr;
+	int clientSize = sizeof(clientAddr);
+	thisSocket = accept(thisSocket, (struct socketaddr *)&clientAddr, (int *)&clientSize);
+
+	if (thisSocket < 0) close_socket_with_error(thisSocket, "\nSocket Connection FAILED!\n", "");
+
+	int newData;
+	size_t BUFFERSIZE = sizeof(int) * 4;
+	char* buffer = calloc(BUFFERSIZE, BUFFERSIZE);
+
+	newData = recv(thisSocket, buffer, BUFFERSIZE, 0);
+
+	printf("\nRecieved data: %s", buffer );
+
+	WSACleanup();
 	return returnStatus;
+}
+
+int close_socket_with_error_num(int socket, const char* message, const int num)
+{
+	char *buffer = calloc(sizeof(128), sizeof(128));
+	sprintf(buffer, message, num);
+	puts(buffer);
+	exit(-1);
+}
+
+int close_socket_with_error(int socket, const char *message, const char *format)
+{
+	vprintf(message, format);
+	if (socket) close(socket);
+	exit(-1);
 }
 
 
